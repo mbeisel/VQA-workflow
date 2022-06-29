1690732f7fae93e1adeba64add7e5f255e0e5fd2
Martin Beisel <beiselmn@gmail.com>
2022-06-29 20:12:32 +0200

    fix ports

--- services/objective-function-service/api/services/helper_service.py
+++ services/objective-function-service/api/services/helper_service.py
@@ -1,16 +1,4 @@
-import qiskit
 from flask import jsonify
-from qiskit.providers.ibmq import IBMQ
-
-
-def getCircuitCharacteristics(circuit, backend=None):
-    if not backend:
-        provider = IBMQ.get_provider(hub="ibm-q")
-        backend = provider.get_backend("ibmq_qasm_simulator")
-        backend = provider.get_backend("ibmq_lima")
-    transpiled = qiskit.compiler.transpile(circuit, backend=backend)
-    return transpiled.width(), transpiled.depth()
-
 
 def bad_request(message):
     response = jsonify({"code": 400, "error": "bad request", "message": message})
